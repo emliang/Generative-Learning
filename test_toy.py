1eb3a65a12be4b9d2946cca64a079b933613b278
Enming Liang <liangenm@mail2.sysu.edu.cn>
2023-10-14 21:29:35 +0800

    update

--- test_toy.py
+++ test_toy.py
@@ -14,7 +14,5 @@ def _main_():
     model_name_list = ['simple', 'hindsight', 'cluster', 'gan', 'diffusion', 'rectified']
     # for model_name in model_name_list:
     #     train_all(data, args, model_name)
-    # for model_name in model_name_list:
-    #     plot_single(data, args, model_name)
     plot_all(data, args, model_name_list)
 _main_()
